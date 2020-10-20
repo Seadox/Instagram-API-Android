f5429a367c0f3fca9ddd0c9df0aeb8949f75cd44
Sean <seanpinchevsky@gmail.com>
2020-10-20 17:43:41 +0300

    Update Client.java

--- instagram_api_android/src/main/java/com/sean/instagram_api_android/Source/Client.java
+++ instagram_api_android/src/main/java/com/sean/instagram_api_android/Source/Client.java
@@ -1,83 +1,97 @@
-package com.sean.instagram_api_android.Source;
-
-import android.content.Context;
-import android.os.Build;
-import android.util.DisplayMetrics;
-import android.util.Log;
+package com.sean.instalive.src;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.util.Locale;
+import java.net.URISyntaxException;
 
 public class Client {
-    public static boolean SendRequest(String endpoint, String post) throws Exception {
-        BufferedReader in = null;
-        Log.i("System", "Debugging... " + Constants.API_URLS[0] + endpoint);
-        try {
-            HttpClient client = new DefaultHttpClient();
-            URI url = new URI(Constants.API_URLS[0] + endpoint);
-            HttpPost request = new HttpPost();
-            request.setHeader("Connection", "close");
-            request.setHeader("Accept", "*/*");
-            request.setHeader("Content-type", Constants.CONTENT_TYPE);
-            request.setHeader("Cookie2", "$Version=1");
-            request.setHeader("Accept-Language", Constants.ACCEPT_LANGUAGE);
-            request.setHeader("User-Agent", "Instagram " + Constants.IG_VERSION +" Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)");
-            request.setHeader("Cookie", Globals.sessionid);
-            request.setURI(url);
+    public static boolean SendRequest(final String endpoint, final String post) {
+        ((Runnable) () -> {
 
-            if (post != null) {
-                StringEntity entity = new StringEntity("ig_sig_key_version=4&signed_body=" + Signatures.HMAC(post, Constants.IG_SIG_KEY) + "." + Signatures.EncodeUrl(post));
-                request.setEntity(entity);
-            }
+            HttpResponse response = null;
 
-            HttpResponse response = client.execute(request);
+            try {
+                HttpClient client = new DefaultHttpClient();
+                URI url = new URI(Constants.API_URLS[0] + endpoint);
 
-            Header[] headers2 = response.getHeaders("Set-Cookie");
-            for (Header header : headers2) {
-                if (header.getValue().split(";")[0].split("=")[0].equals("sessionid"))
-                    Globals.sessionid = header.getValue().split(";")[0];
-                if (header.getValue().split(";")[0].split("=")[0].equals("csrftoken"))
-                    Globals.csrftoken = header.getValue().split(";")[0].split("=")[1];
-            }
+                //POST request
+                if (post != null) {
+                    HttpPost request = new HttpPost();
+                    request.setHeader("Connection", "Keep-Alive");
+                    request.setHeader("Accept", "*/*");
+                    request.setHeader("Content-type", Constants.CONTENT_TYPE);
+                    request.setHeader("Cookie2", "$Version=1");
+                    request.setHeader("Accept-Language", Constants.ACCEPT_LANGUAGE);
+                    request.setHeader("User-Agent", Globals.userAgent);
+                    request.setHeader("Cookie", Globals.sessionid);
+                    request.setHeader("X-IG-Connection-Type", Constants.X_IG_Connection_Type);
+                    request.setURI(url);
 
-            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuffer buffer = new StringBuffer("");
+                    StringEntity entity = new StringEntity("ig_sig_key_version=4&signed_body=" + Signatures.HMAC(post, Constants.IG_SIG_KEY) + "." + Signatures.EncodeUrl(post));
+                    request.setEntity(entity);
 
-            String l = "";
-            String nl = System.getProperty("line.separator");
-            while ((l = in.readLine()) != null){
-                buffer.append(l).append(nl);
-            }
-            in.close();
-            Globals.LastResponse = buffer.toString();
-            return true;
-
-        }finally {
-            if (in != null){
-                try {
-                    in.close();
-                    return true;
-                }catch (Exception e){
-                    e.printStackTrace();
+                    response = client.execute(request);
                 }
+                //GET request
+                else{
+                    HttpGet request = new HttpGet();
+                    request.setHeader("Connection", "Keep-Alive");
+                    request.setHeader("Accept", "*/*");
+                    request.setHeader("Content-type", Constants.CONTENT_TYPE);
+                    request.setHeader("Cookie2", "$Version=1");
+                    request.setHeader("Accept-Language", Constants.ACCEPT_LANGUAGE);
+                    request.setHeader("User-Agent", "Instagram " + Constants.IG_VERSION + " Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)");
+                    request.setHeader("Cookie", Globals.sessionid);
+                    request.setHeader("X-IG-Connection-Type", Constants.X_IG_Connection_Type);
+                    request.setURI(url);
+
+                    response = client.execute(request);
+                }
+
+                HttpEntity entity = response.getEntity();
+                InputStream is = entity.getContent();
+
+                Header[] headers2 = response.getHeaders("Set-Cookie");
+                for (Header header : headers2) {
+                    if (header.getValue().split(";")[0].split("=")[0].equals("sessionid"))
+                        Globals.sessionid = header.getValue().split(";")[0];
+                    if (header.getValue().split(";")[0].split("=")[0].equals("csrftoken"))
+                        Globals.csrftoken = header.getValue().split(";")[0].split("=")[1];
+                }
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+
+                String l = "";
+                while ((l = in.readLine()) != null) // Read line by line
+                    sb.append(l).append("\n");
+
+                is.close();
+
+                Globals.LastResponse = String.valueOf(sb);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
+        }).run();
+        return true;
     }
 
-    public static String m8457b(Context context) {
-        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int i = (int) (displayMetrics.density * 160.0f);
-        int i2 = displayMetrics.heightPixels;
-        int i3 = displayMetrics.widthPixels;
-        return String.format("%s Android (%s/%s; %s; %s; %s; %s; %s; %s; %s_%s)", "Instagram 9.2.0", Build.VERSION.RELEASE, Build.VERSION.SDK, i + "dpi", i2 + "x" + i3, Build.MODEL, Build.MANUFACTURER, Build.BRAND, Build.DEVICE, Locale.getDefault().getLanguage(), Locale.getDefault().getCountry());
-    }
+
 }
